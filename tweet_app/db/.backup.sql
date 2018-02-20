1a2f8ee66516588c0c8c12fdcec6cb34f18f0169
Takahashi Rina <19r.takahashi90@gmail.com>
2018-02-20 09:26:35 +0900

    Tweet App

---
+++ tweet_app/db/.backup.sql
@@ -0,0 +1,13 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170215053008');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2018-02-12 03:09:23.622446','2018-02-12 03:09:23.622446');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'今日からProgateでRailsの勉強するよー！','2018-02-12 09:20:31.759704','2018-02-12 09:20:31.759704');
+INSERT INTO posts VALUES(2,'投稿一覧ページ作成中！','2018-02-12 09:21:18.454667','2018-02-12 09:21:18.454667');
+INSERT INTO posts VALUES(3,'やっぱりプログラミングは楽しい！','2018-02-12 09:31:03.538049','2018-02-12 09:31:03.538049');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',3);
+COMMIT;
